e6ad6393642ce8a7807cba9f6dbe1a3c33370f91
ebeer12 <ebeer19@gmail.com>
2014-05-14 17:43:51 +0200

    Wprowadzenie funkcji dodawania i odejmowania

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,10 @@
-print("Podaj dwie liczby. Kalkulator doda je do siebie.")
 a = input()
 b = input()
-result = int(a) + int(b)
+task = input()
+if(task == '+'):
+    result = int(a) + int(b)
+elif(task == '-'):
+    result = int(a) - int(b)
+else:
+    result = "BŁĄÐ"
 print(result)
