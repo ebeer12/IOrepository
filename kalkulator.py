03222cadd92a71152e6ecec44e3fb7221a17981c
ebeer12 <ebeer19@gmail.com>
2014-05-14 18:00:35 +0200

    Dodanie kilku przydatnych poprawek kalkulatora

--- kalkulator.py
+++ kalkulator.py
@@ -1,11 +1,16 @@
-print("Podaj dwie liczby i działanie. Kalkulator doda/odejmie je od siebie.")
+print("Podaj dwie liczby i działanie. Kalkulator wykona na nich podane działanie.")
 a = input()
 b = input()
+print("Podaj działanie (+,-,*,/)")
 task = input()
 if(task == '+'):
     result = int(a) + int(b)
 elif(task == '-'):
     result = int(a) - int(b)
+elif(task == '*'):
+    result = int(a) * int(b)
+elif(task == '/'):
+    result = int(a) / int(b)        
 else:
     result = "BŁĄÐ"
 print(result)
